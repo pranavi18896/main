dbd4baed74a026b0b06932b4776ed9712fce9050
pranavi <pravani@gmail.com>
2023-04-24 21:56:58 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,10 @@
+class hello{
+public static void main(String args[])
+{
+System.out.println("hello worldS");
+System.out.println("hello world\nwith\nlove\nyours\npranavi");
+System.out.print("*\n**\n***\n***");
+int a=25,b=10;
+
+}
+}
